fe770f5201cecc1d5302c34cac4e2d8fdf683789
maheshksingh <mahesh.ksingh711@gmail.com>
2021-05-24 18:53:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, abort, url_for, json, jsonify, Flask, Response, send_from_directory, send_file, Flask, make_response, request
+from flask import render_template
 import pandas as pd
 import json
 import os
