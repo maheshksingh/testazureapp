23793fe47c4e53412686606ac2de8acd67bac991
maheshksingh <mahesh.ksingh711@gmail.com>
2021-08-02 14:37:57 +0530

    Trying SQL Server Native Client 11.0

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from rank_bm25 import BM25Okapi
 app = Flask(__name__)
 
 # retrieve datasettry:
-cnxn = pyodbc.connect('ODBC Driver 17 for SQL Server};'
+cnxn = pyodbc.connect('SQL Server Native Client 11.0};'
 'SERVER=tcp:segmentcodedbserver.database.windows.net,1433;'
 'DATABASE=segmentcodedb;UID=segmentcode;PWD=Mahesh143;')
 
