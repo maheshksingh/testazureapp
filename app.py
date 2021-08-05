75a35ae84b7215dc96d745ffb393e5a8ee949b3c
maheshksingh <mahesh.ksingh711@gmail.com>
2021-08-05 13:03:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,16 @@ def s1():
     elif request.method=='POST':
         pno=request.form.get("pno")
         # retrieve dataset:
-        cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
-            'SERVER=tcp:segmentcodedbserver.database.windows.net,1433;'
-            'DATABASE=segmentcodedb;UID=segmentcode;PWD=Mahesh143;')
+        for i in range(0,3):
+            while True:
+                try:
+                    cnxn = pyodbc.connect('DRIVER={SQL Server};'
+                    'SERVER=tcp:segmentcodedbserver.database.windows.net,1433;'
+                    'DATABASE=segmentcodedb;UID=segmentcode;PWD=Mahesh143;')
+                except:
+                    print("Couldn't connect to db, trying again")
+                    continue
+                break
         cursor = cnxn.cursor()
         cursor.execute("SELECT Partno,Name, Demarcation,Characteristics, Functiongroup,PartType,Weight,Material,Length,Width,Height,ReferencePart,PrecedingPart,SuccedingPart,SegmentCode,SegmentDescription FROM segmentjune03 WHERE Partno="+pno) 
         res = cursor.fetchone()
@@ -116,9 +123,16 @@ def s2():
             if i!="":
                 query+=i+" "
         # retrieve dataset:
-        cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
-            'SERVER=tcp:segmentcodedbserver.database.windows.net,1433;'
-            'DATABASE=segmentcodedb;UID=segmentcode;PWD=Mahesh143;')
+        for i in range(0,3):
+            while True:
+                try:
+                    cnxn = pyodbc.connect('DRIVER={SQL Server};'
+                    'SERVER=tcp:segmentcodedbserver.database.windows.net,1433;'
+                    'DATABASE=segmentcodedb;UID=segmentcode;PWD=Mahesh143;')
+                except:
+                    print("Couldn't connect to db, trying again")
+                    continue
+                break
         SQL_Query = pd.read_sql_query("select Name, Demarcation,Characteristics, Functiongroup,PartType,Weight,Material,Length,Width,Height,SegmentCode,SegmentDescription from segmentjune03 WHERE Name LIKE '%"+name+"%'", cnxn)
         df = pd.DataFrame(SQL_Query, columns=['Name','Demarcation','Charactersistics','Functiongroup','PartType','Weight','Material','Length','Width','Height','SegmentCode','SegmentDescription'])
         df.reset_index(level=0, inplace=True)
